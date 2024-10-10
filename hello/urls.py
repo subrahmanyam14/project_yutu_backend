3ac7aca42331d4f74937096c7ff3b5f64d0dee3e
Vagdevi <surarapuvagdevi@gmail.com>
2024-10-10 14:32:21 +0530

    updated

--- hello/urls.py
+++ hello/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import hello, translate_text
 
 urlpatterns = [
-    path('translate/', translate_text, name='translate_text'),
-    path('hello/', hello, name='hello'),
+    path('translate', translate_text, name='translate_text'),
+    path('hello', hello, name='hello'),
 ]
